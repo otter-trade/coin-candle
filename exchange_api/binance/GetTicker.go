33f899e422dbef072cb40f8948d5dd4617c07601
墨七 <mo7@mo7.cc>
2024-06-22 23:40:20 +0800

    休息一会儿

---
+++ exchange_api/binance/GetTicker.go
@@ -0,0 +1,10 @@
+package binance
+
+func GetTicker() {
+	/*
+		 	resData, err := binance.FetchBinancePublic(binance.FetchBinancePublicOpt{
+				Path:   "/api/v3/ticker/24hr",
+				Method: "get",
+			})
+	*/
+}
---
+++ exchange_api/okx/GetTicker.go
@@ -0,0 +1,13 @@
+package okx
+
+func GetTicker() {
+	/*
+		resData, err := mOKX.FetchOKX(mOKX.OptFetchOKX{
+			Path: "/api/v5/market/tickers",
+			Data: map[string]any{
+				"instType": "SPOT",
+			},
+			Method: "get",
+		})
+	*/
+}

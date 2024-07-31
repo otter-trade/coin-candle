a0a61c7dc1cb3aef81ffce74197422e22f5dd66d
墨七 <meichangliang@outlook.com>
2024-07-31 14:42:32 +0800

    下单结余应该是搞定了~

--- global/types-mock-trade.go
+++ global/types-mock-trade.go
@@ -139,7 +139,7 @@ type NewPositionType struct {
 	TradeType string // 交易种类，可选值 TradeTypeList
 	Leverage  string // 杠杆倍率，缺省值 1 ，只有 TradeMode = SWAP 时有效
 	Side      string // 下单方向 Buy , Sell , 只有 TradeMode = SWAP 时有效
-	Amount    string // 下单金额，不可超过账户结余，传 0 则为平仓
+	Amount    string // 下单金额，不可超过账户结余，传 0 则为空仓
 }
 
 type UpdatePositionOpt struct {
